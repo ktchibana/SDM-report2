83591e68819f8828475e35d4670a2a2025cfa81f
ktchibana <ch.kit759@gmail.com>
2026-02-03 07:00:35 +0900

    testrunnerの修正: 多重実行を防止

--- testrunner.py
+++ testrunner.py
@@ -3,21 +3,22 @@
 import unittest
 import sys
 
-loader = unittest.TestLoader()
+if __name__ == '__main__': # このファイルが直接実行された場合のみ実行
+    loader = unittest.TestLoader()
 
 
-#Find the test files in the current directory
+    #Find the test files in the current directory
 
-tests = loader.discover('.')
+    tests = loader.discover('.')
 
-#Specify the level of information provided by the test runner
+    #Specify the level of information provided by the test runner
 
-testRunner = unittest.runner.TextTestRunner(verbosity=2)
+    testRunner = unittest.runner.TextTestRunner(verbosity=2)
 
-results=testRunner.run(tests)
+    results=testRunner.run(tests)
 
-#print( "results: %s" % results )
-#print( "results.wasSuccessful: %s" % results.wasSuccessful() )
-if results.wasSuccessful()==False:
-    sys.exit(1)
+    #print( "results: %s" % results )
+    #print( "results.wasSuccessful: %s" % results.wasSuccessful() )
+    if results.wasSuccessful()==False:
+        sys.exit(1)
 
